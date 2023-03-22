b356dc8ef349e68e5821283a6909763e6cdbf0e0
elie-atia <elie-atia@hotmail.fr>
2023-03-23 00:33:44 +0200

    feat: setting up base web

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { Provider as StyletronProvider } from 'styletron-react';
+import { LightTheme, BaseProvider } from 'baseui';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    //Styletron is the style engine for base-web
+    //BaseProvider will save the style state and LightTheme
+    <StyletronProvider value={engine}>  
+      <BaseProvider theme={LightTheme}>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </BaseProvider>
+    </StyletronProvider>
   );
 }
 
