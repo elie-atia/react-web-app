caaed3d1871e2fc036755ac5892a8ae4b720e4f8
elie-atia <elie-atia@hotmail.fr>
2023-03-23 16:32:26 +0200

    style:put all the menu item into a variable

--- src/containers/Layout/Sidebar.js
+++ src/containers/Layout/Sidebar.js
@@ -2,49 +2,46 @@ import { HomeIcon } from '../../assets/images/HomeIcon';
 import { SidebarWrapper, MenuWrapper, NavLink, Svg } from './Sidebar.style';
 
 export default function Sidebar() {
+
+  const sidebarMenus = [
+    {
+      name: 'Home',
+      path: './',
+      exact: true,
+      icon: <HomeIcon />,
+    },
+    {
+      name: 'Page 1',
+      path: '/page1',
+      exact: false,
+      icon: <HomeIcon />,
+    },
+    {
+      name: 'Page 2',
+      path: './page2',
+      exact: false,
+      icon: <HomeIcon />,
+    }
+  ];
   return (
     <SidebarWrapper >
  <MenuWrapper>
+        {sidebarMenus.map((menu,index)=>(
+        <NavLink  
+        to= {menu.path}
+        key={index}
+        exact={menu.exact}
+        activeStyle={{
+          color: '#00C58D',
+          backgroundColor: '#f7f7f7',
+          borderRadius: '50px 0 0 50px',
+        }}
+      >
+      <Svg> {menu.icon}</Svg> 
+        {menu.name}
+      </NavLink>))
+        }
         
-          <NavLink  
-            to= '/'
-            key={1}
-            exact={true}
-            activeStyle={{
-              color: '#00C58D',
-              backgroundColor: '#f7f7f7',
-              borderRadius: '50px 0 0 50px',
-            }}
-          >
-          <Svg> <HomeIcon/></Svg> 
-            Home
-          </NavLink>
-          <NavLink
-            to= '/page1'
-            key={2}
-            exact={true}
-            activeStyle={{
-              color: '#00C58D',
-              backgroundColor: '#f7f7f7',
-              borderRadius: '50px 0 0 50px',
-            }}
-          >
-          <Svg> <HomeIcon /></Svg> 
-            Page 1
-          </NavLink>
-          <NavLink
-            to= '/page2'
-            key={3}
-            exact={true}
-            activeStyle={{
-              color: '#00C58D',
-              backgroundColor: '#f7f7f7',
-              borderRadius: '50px 0 0 50px',
-            }}
-          >
-          <Svg> <HomeIcon /></Svg> 
-            Page 2
-          </NavLink>
       </MenuWrapper>
     </SidebarWrapper>
   );
