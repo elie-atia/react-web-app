b2039db98ad7b9d3bda8761b3493f12d8e04623e
elie-atia <elie-atia@hotmail.fr>
2023-03-23 18:52:46 +0200

    feat:add a redux store+2 reducers and somes dispatch func.

---
+++ src/state/MarketSlice/MarketSlice.js
@@ -0,0 +1,25 @@
+import {createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  market_pairs: [],
+  active_market: [],
+  status: 'idle',
+};
+  
+export const marketSlice = createSlice({
+  name: 'market',
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+    update_market_pairs: (state = {}, action) => {
+      state.market_pairs = action.payload
+    },
+    set_active_market: (state = {}, action) => {
+      state.active_market = action.payload
+    },
+  },
+});
+
+export const { update_market_pairs, set_active_market } = marketSlice.actions;
+
+export default marketSlice.reducer;
