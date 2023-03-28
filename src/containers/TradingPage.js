7553dda77fb9d9e36963c58a4feb563664bcfb9b
elie-atia <elie-atia@hotmail.fr>
2023-03-28 14:02:29 +0300

    add table components to display recent transactions (using Material-UI)

--- src/containers/TradingPage.js
+++ src/containers/TradingPage.js
@@ -1,22 +1,29 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Line } from 'react-chartjs-2';
 import MyChart from '../components/Chart/MyChart';
-import { Button, TextField } from '@mui/material';
+import { Button, TextField, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 
-const TradingPage  = () => {
-    const [priceData, setPriceData] = useState({
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [
-          {
-            label: 'Price',
-            data: [65, 59, 80, 81, 56, 55, 40],
-            fill: false,
-            backgroundColor: 'rgba(75,192,192,0.4)',
-            borderColor: 'rgba(75,192,192,1)',
-          },
-        ],
-      });
-      const [quantity, setQuantity] = useState('');
+const TradingPage = () => {
+  const [priceData, setPriceData] = useState({
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'Price',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+      },
+    ],
+  });
+  const [transactionData, setTransactionData] = useState([
+    { id: 1, type: 'buy', amount: 0.1, price: 50000, date: '2022-04-01' },
+    { id: 2, type: 'sell', amount: 0.05, price: 45000, date: '2022-04-02' },
+    { id: 3, type: 'buy', amount: 0.2, price: 48000, date: '2022-04-03' },
+    { id: 4, type: 'sell', amount: 0.15, price: 52000, date: '2022-04-04' },
+  ]);
+
+  const [quantity, setQuantity] = useState('');
 
   const handleBuy = () => {
     // Logic to handle buying cryptocurrency
@@ -27,14 +34,14 @@ const TradingPage  = () => {
     // Logic to handle selling cryptocurrency
     console.log(`Selling ${quantity} cryptocurrency`);
   };
-  
+
   return (
     <div>
-    <div>
-    <MyChart {...priceData} /> 
-    </div>
+      <div>
+        <MyChart {...priceData} />
+      </div>
 
-    <TextField
+      <TextField
         id="quantity"
         label="Quantity"
         value={quantity}
@@ -46,10 +53,34 @@ const TradingPage  = () => {
       <Button variant="contained" color="secondary" onClick={handleSell}>
         Sell
       </Button>
-      </div>
-     );
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>Type</TableCell>
+              <TableCell>Amount</TableCell>
+              <TableCell>Price</TableCell>
+              <TableCell>Date</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {transactionData.map((transaction) => (
+              <TableRow key={transaction.id}>
+                <TableCell>{transaction.id}</TableCell>
+                <TableCell>{transaction.type}</TableCell>
+                <TableCell>{transaction.amount}</TableCell>
+                <TableCell>{transaction.price}</TableCell>
+                <TableCell>{transaction.date}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 };
 
-export default TradingPage ;
+export default TradingPage;
 
 
