619a42e30f7e51ec34cdad68c15096f21847c6ff
elie-atia <elie-atia@hotmail.fr>
2023-03-30 13:12:50 +0300

    add a simple express server connect to firestore (backend)

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,9 +1,33 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+const db = admin.firestore();
+
+app.use(cors({ origin: true }));
+
+app.get('/hello', (req, res) => {
+    const city = 'paris';
+    //   Add a new document with a generated id and the city name.
+    db.collection("cities").add({
+        name: city
+    })
+        .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+            res.send(`Hello from ${city} added to Firestore!`);
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+            res.status(500).send('Error adding document to Firestore.');
+        });
+});
+// Start the server
+app.listen(5000, () => {
+    console.log('Server running on http://localhost:5000');
+});
+
+exports.api = functions.https.onRequest(app);
